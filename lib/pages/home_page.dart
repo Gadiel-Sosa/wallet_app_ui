7e93352b43ad63767f259102d691bbb65437c86d
Gadiel Sosa <sosachanantonio0@gmail.com>
2025-04-10 14:23:47 -0600

    More icons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:wallet_app_ui/utils/my_button.dart';
 import 'package:wallet_app_ui/utils/my_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -30,14 +31,14 @@ class _HomePageState extends State<HomePage> {
                       Text(
                         'My ',
                         style: TextStyle(
-                          fontSize: 28,
+                          fontSize: 35,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                       Text(
                         'Card',
                         style: TextStyle(
-                          fontSize: 28,
+                          fontSize: 35,
                         ),
                       ),
                     ],
@@ -47,7 +48,10 @@ class _HomePageState extends State<HomePage> {
                       padding: const EdgeInsets.all(6.0),
                       decoration: BoxDecoration(
                           color: Colors.pink[400], shape: BoxShape.circle),
-                      child: Icon(Icons.add)),
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.black,
+                      )),
                 ],
               ),
             ),
@@ -59,6 +63,7 @@ class _HomePageState extends State<HomePage> {
             // ignore: sized_box_for_whitespace
             Container(
               height: 200,
+              width: 440,
               child: PageView(
                 scrollDirection: Axis.horizontal,
                 controller: _controller,
@@ -97,14 +102,90 @@ class _HomePageState extends State<HomePage> {
               controller: _controller,
               count: 3,
               effect: ExpandingDotsEffect(activeDotColor: Colors.grey.shade700),
-            )
+            ),
+            SizedBox(
+              height: 25,
+            ),
 
             //* 3 botones --> send + pay + bill
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  //* Send button
+                  MyButton(
+                      myImagePath: 'lib/wallet/send_money.png',
+                      imageText: 'Send'),
+                  //* pay button
+                  MyButton(
+                      myImagePath: 'lib/wallet/credit_card.png',
+                      imageText: 'Pay'),
+                  //* bill button
+                  MyButton(
+                      myImagePath: 'lib/wallet/bill.png', imageText: 'Bill'),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
 
             //* stats + trasnsactions
+            Padding(
+              padding: const EdgeInsets.all(25),
+              child: Column(
+                children: [
+                  //* Statistics
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          //* Icon
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                borderRadius: BorderRadius.circular(12)),
+                            height: 80,
+                            padding: const EdgeInsets.all(12),
+                            child: Image.asset('lib/status/statistic.png'),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Sattistics',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                'Payments and Icome',
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.grey[700]),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Icon(Icons
+                          .arrow_forward_ios) //*Me quedé aquí antes de encapsular todo esto
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
+
+//* me quedé aquí al crear las tarjetas
---
+++ lib/utils/my_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String myImagePath;
+  final String imageText;
+
+  const MyButton(
+      {super.key, required this.myImagePath, required this.imageText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 90,
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade400,
+                  blurRadius: 40,
+                  spreadRadius: 10,
+                )
+              ]),
+          child: Center(
+            child: Image.asset(myImagePath),
+          ),
+        ),
+        SizedBox(
+          height: 12,
+        ),
+        Text(
+          imageText,
+          style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[800]),
+        )
+      ],
+    );
+  }
+}
